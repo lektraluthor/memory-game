45a471b646ca1129590fbe44f8f9f79e5c526c8d
lektra <annvolkovych@gmail.com>
2018-10-09 22:14:57 +0300

    Board lock
    Locked the board to avoid two sets of cards being turned at the same time

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -31,10 +34,14 @@ function disableCards() {
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');                
-    }, 1000)
+        secondCard.classList.remove('flip');   
+        
+        lockBoard = false;
+    }, 1000);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
